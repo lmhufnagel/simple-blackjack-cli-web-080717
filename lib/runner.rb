5cb140481d9e988bb1c3a453a2f8145ef16db8af
Lisa Hufnagel <lmhufnagel@gmail.com>
2017-07-31 01:25:40 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,20 @@
 require_relative "blackjack.rb"
 
+
 runner
+
+
+
+# put below at bottom of blackjack.rb!!
+def runner
+  # code runner here
+  welcome
+  deal_card
+  display_card_total
+  prompt_user
+  get_user_input
+  end_game
+  initial_round
+  hit?
+  invalid_command
+end
